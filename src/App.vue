910a5dda08dd29f3f0cc65ad32057f98829b12d8
Laurita23 <fernandez.hujon21@zaragoza.salesianos.edu>
2023-07-23 20:07:34 +0200

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Title from './components/Title.vue'
+</script>
+
+<template>
+  <header >
+
+    <div >
+      <Title msg="Todo List" />
+    </div>
+  </header>
+
+  <main>
+    <div>Hello World!</div>
+  </main>
+</template>
+
+<style scoped>
+header {
+  max-width: none;
+}
+
+@media (min-width: 1024px) {
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div class="container">
+    <img alt="Vue logo" src="../assets/logo.svg" width="125" height="125" />
+    <h1 class="green">{{ msg }}</h1>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+}
+
+.container h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  padding: 1rem 0 0 1rem;
+}
+</style>
