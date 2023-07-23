f1420ff9b38d28f6db7efee67eb862f2f8d12b32
Laurita23 <fernandez.hujon21@zaragoza.salesianos.edu>
2023-07-23 22:30:05 +0200

    Añadido componente Todo y modificado el css del header

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <script setup lang="ts">
 import Title from './components/Title.vue'
+import Todo from './components/Todo.vue'
 </script>
 
 <template>
-  <header >
-
-    <div >
-      <Title msg="Todo List" />
-    </div>
-  </header>
+    <header >
+      
+      <div >
+        <Title msg="Todo List" />
+      </div>
+    </header>
 
   <main>
-    <div>Hello World!</div>
+    <Todo />
   </main>
 </template>
 
 <style scoped>
 header {
-  max-width: none;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
 }
-
 @media (min-width: 1024px) {
 
   .logo {
